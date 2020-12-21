2106754c339af4c5037003582575044261b6f6aa
Henrique <Carlos Henrique>
2020-12-20 21:30:09 -0300

    modified src/pages/Home/index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import React,{ useState } from 'react';
 import Nav from '../../components/Nav/index'
 
 import ImagnsBooking from '../../assets/imageCalendar.svg'
+import Background from '../../assets/bg-2.svg'
 
 import ModalPlaces from  '../../components/Modal/Modal_Places/index'
 import ModalContentPlaces from '../../components/Modal/Modal_Places/modal.context'
@@ -15,13 +16,16 @@ const Home = () => {
 
     return (
         <>
-        <div>
+        <img className="background" src={Background} alt="image_background"/>
+        <div> 
             < Nav />
         </div>
+       
         <p className="text_main">Brasil Bookings </p>
         <h5>Pesquise centenas de lugares de uma só vez e encontre Restaurantes, Hotéis, Bares para você fazer sua reserva na comodidade de sua casa.</h5>
         <button className="button_home" onClick={() => setModalOpenPlaces(true)}>Tipos de Reservas</button>
-        <img className="main_image" src={ImagnsBooking} alt="image"/>
+         <img className="main_image" src={ImagnsBooking} alt="image_booking"/>
+       
 
 
         <ModalPlaces modalOpen={modalOpenPlaces}>
