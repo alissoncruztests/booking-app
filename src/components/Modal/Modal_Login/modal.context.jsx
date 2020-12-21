ef2b1f0f1e637db710738ba96ef956523482c84a
Henrique <Carlos Henrique>
2020-12-20 21:33:45 -0300

    modified modal src/components/Modal

--- src/components/Modal/Modal_Login/modal.context.jsx
+++ src/components/Modal/Modal_Login/modal.context.jsx
@@ -1,84 +1,95 @@
-import React,{useState} from 'react';
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import { Link} from 'react-router-dom'
 import { message } from 'antd'
 
-import axios from 'axios'
 import { motion } from 'framer-motion'
 import { FiX } from 'react-icons/fi'
 import { FaFacebook } from 'react-icons/fa'
 import { FcGoogle } from "react-icons/fc"
+
+import axios from 'axios'
+import { login } from "../../../services/auth";
 
 
 import './styles.css'
 
 
 const ModalSignUp = ({setModalOpen}) => {
-        
-    const [ email, setEmail] = useState('')
-    const [ password, setPassword ] = useState('')
-  
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     const handleLogin = async (e) => {
         e.preventDefault()
 
-        const data = {email, password }
+        const data = { username, password }
+        console.log(data)
+        const requestInfo = {
+            body: data,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        }
         const key = 'updatable';
 
-            if(email !== '' && password !== ''){
-                try {
-                    const response = await axios.post('https://bookings-madein-pi.herokuapp.com/booking/v1/login', data)
-                    console.log(response.data)
-                    console.log(response.statusText)
-                    if(response.status === 200){
-
-                        message.info({ content: 'Usuário logado, tente com outro email.', key, duration: 2 })
+        if(!username || !password) {
+            message.info({ content: 'Preencha todos os campos.', key, duration: 3.5 });
+        }
+        else {
+            try {
+                const response = await axios.post('https://bookings-madein-pi.herokuapp.com/booking/v1/login', requestInfo )
+                .then(response => {
+                    if(response.ok) {
+                        return response.json()
                     }
-                    else{
-                        message.loading({ content: 'Loading...', key });
-                        setTimeout(() => {
-                            message.success({ content: 'Usuário cadastrado com sucesso.', key, duration: 3 });
-                        }, 1000);
+                })
+                .then(token => console.log(token))
 
-                     }   
+                login(response.data.token)
+                
 
-                }catch{
-                    message.warning({ content:'Erro, por favor tente novamente...', duration: 3 });
-                }
-
-            }else {
-                message.info({ content: 'Preencha todos os campos.', key, duration: 3.5 });
-            }   
-        
+            } catch (err) {
+                message.warning({ content:"Houve um problema com o login, verifique suas credenciais. T.T"
+                , duration: 3 });
+            }
+        }
     }
-    return (
+    return(
     <motion.div
         initial={{opacity:0 ,y: 0.1}}
         animate={{opacity: 1, y: 0}}
         exit={{opacity: 0, y:'50%'}}
     >
-        
 
     <div className="container_modal_signup">
         <button className="button_close_signup" onClick={ ()=> setModalOpen(false)}>< FiX style={{width: 40, height: 20,fontWeight: 800}}/></button>
-        <form onSubmit={handleLogin}>
-            <div>
+        <div>
+            <form onSubmit={handleLogin}>
+
                 <h1>Entrar</h1>
                 <h4>Informe email e senha</h4>
-                
-                <input className="signup_email" type="text" placeholder="email" 
-                value={email}
-                onChange={ e => setEmail(e.target.value)} 
-                />
-                <input className="signup_password"type="text" placeholder="senha" 
-                value={password}
-                onChange={ e => setPassword(e.target.value)} 
-                />
 
-                <button className="signup_button_entrar"  type="submit">Entrar</button>
+                <input 
+                className="signup_email" 
+                type="text" 
+                placeholder="email" 
+                value={username}
+                onChange={ e => setUsername(e.target.value)} 
+                />
+                <input 
+                className="signup_password"
+                type="password" 
+                placeholder="senha" 
+                value={password}
+                onChange={ e => setPassword(e.target.value)} />
+
+                <button className="signup_button_entrar">Entrar</button>
 
                 <p className="cadastre-se">Ainda não tem uma conta? 
-                <Link to='/'>
+                  <Link to='/'>
                     Cadastre - se
-                </Link></p>
+                  </Link></p>
                 <p>ou</p>
 
                 <button className="signup_button_facebook"> Continuar com Facebook</button>
@@ -89,10 +100,11 @@ const ModalSignUp = ({setModalOpen}) => {
                 <div className="icon_google">
                     <FcGoogle />
                 </div>
-            </div>
-        </form>
-   </div>
+            </form>
+        </div>
+        </div>
     </motion.div>
-)}
+    )
+}
 
 export default ModalSignUp;
